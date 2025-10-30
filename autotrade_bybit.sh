f03b31aa78fa98b30bee81ceec98194a06efef39
Marcos Lima <marcosrioj@gmail.com>
2025-10-30 16:25:35 -0700

    up

--- autotrade_bybit.sh
+++ autotrade_bybit.sh
@@ -3,6 +3,5 @@
 source .venv/bin/activate
 
 # Run cryptoforecast.py passing all arguments given to this script
-export BYBIT_API_KEY=\"<your_test_api_key>\"
-export BYBIT_API_SECRET=\"<your_test_api_secret>\"
-python3 watchlist_notify.py --email-to marcosrioj@gmail.com
+
+python3 autotrade_bybit.py "$@"
